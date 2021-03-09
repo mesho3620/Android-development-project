0e2c29b755e6c33e5afeb5a631016e74f0e64b8b
Ahmad-Mostafa-Kamel <45707865+Ahmad-Mostafa-Kamel@users.noreply.github.com>
2021-03-09 17:11:59 +0200

    settings without maps

---
+++ settings.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'bottom_bar.dart';
+import 'editprofile.dart';
+
+class GeneralSettings extends StatelessWidget {
+  @override
+  // This widget is the root of your application.
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: new AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.deepOrange,
+        title: new Text(
+          "Settings",
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    onTap: () {
+                      //for editing the profile
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfilePage()));
+                    },
+                    title: Text(
+                      "Adham Hamzawy",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.redAccent,
+                      child: Text('AH'),
+                    ),
+                    trailing: Icon(
+                      Icons.edit,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                Card(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 8.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: double.infinity,
+                        height: 1.0,
+                        color: Colors.grey.shade400,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 5.0,
+                        color: Colors.grey.shade400,
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.map,
+                          color: Colors.black,
+                        ),
+                        title: Text("Change Location"),
+                        trailing: Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Colors.black,
+                        ),
+                        onTap: () {
+                          //change location
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+              ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepOrange,
+        child: Icon(Icons.home),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomBar(),
+    );
+  }
+
+  Container _buildDivider() => Container();
+}
